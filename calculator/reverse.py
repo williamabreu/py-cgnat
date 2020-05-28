1f835575556fa393724992bb76b8690ea5becabc
williamabreu <contato@williamabreu.net>
2020-05-28 19:37:51 -0300

    Refactored cgnat_reverse code for make it reusable

--- calculator/reverse.py
+++ calculator/reverse.py
@@ -1,47 +1,45 @@
-import sys
-import ipaddress
-
-# uso:
-# python3 cgnat_reverse.py <CGNAT-SUBNET> <PUBLIC-SUBNET> <PUBLIC-IP> <PUBLIC-PORT>
-# python3 cgnat_reverse.py 100.65.12.0/22 177.66.58.160/27 177.66.58.170 5220
+from utils.vlsm import split_subnet
+from ipaddress import IPv4Address, IPv4Network
 
 
-def split_subnet(subnet, netmask):
-    if subnet.netmask == netmask:
-        return [subnet]
-    else:
-        branches = list(subnet.subnets())
-        return split_subnet(branches[0], netmask) + split_subnet(branches[1], netmask)
+def cgnat_reverse(private_net: IPv4Network, public_net: IPv4Network, public_ip: IPv4Address, public_port: int) -> dict:
+    """Calculates the private IP and port range from public IP given.
 
+    Args:
+        private_net: Private address pool from CGNAT shared space address.
+        public_net: Public adddress pool target from netmap.
+        public_ip: Unique public IP from CGNAT to be converted to the private one.
+        public_port: Target port for the network translation.
+    
+    Returns:
+        Dict containing the private_ip and port_range for the query.
+    
+    Raises:
+        ValueError: When the public IP is out of the public net given.
+        ValueError: When the networks given do not satisfy the 1:32 ratio.
+    """
+    
+    if public_ip not in public_net:
+        raise ValueError('Public IP is out of the network given')
 
-def cgnat_reverse(cgnat_net, public_ip, client_ip, client_port):
-    private_ips = split_subnet(cgnat_net, public_ip.netmask)
+    if public_net.prefixlen - private_net.prefixlen != 5:
+        raise ValueError('Only works to netmaps for 1:32 CGNAT ratio')
 
-    index = None
+    private_ips = split_subnet(private_net, public_net.netmask)
+    index = None # to discover the port range
 
     for i in range(32):
         port_base = 1536 + 2000 * i
         port_range = (port_base, port_base + 1999)
-        if port_range[0] <= client_port <= port_range[1]:
+        if port_range[0] <= public_port <= port_range[1]:
             index = i
             break
 
     pool = private_ips[index]
-    delta = int(client_ip) - int(public_ip.network_address)
-    client_private_ip = ipaddress.IPv4Address(int(pool.network_address) + delta)
+    delta = int(public_ip) - int(public_net.network_address)
+    private_ip = IPv4Address(int(pool.network_address) + delta)
 
-    return client_private_ip, client_ip, port_range
-
-
-if __name__ == '__main__':
-    cgnat_net = ipaddress.IPv4Network(sys.argv[1])
-    public_ip = ipaddress.IPv4Network(sys.argv[2])
-    client_ip = ipaddress.IPv4Address(sys.argv[3])
-    client_port = int(sys.argv[4])
-    
-    client_private_ip, client_ip, port_range = cgnat_reverse(cgnat_net, public_ip, client_ip, client_port)
-
-    print('IP CLIENTE:', client_private_ip)
-    print('IP PÚBLICO:', client_ip)
-    print('PORTAS:', port_range)
-
+    return {
+        'private_ip': private_ip,
+        'port_range': port_range
+    }        
