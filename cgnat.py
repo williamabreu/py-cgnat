f5d402a6769535716030a46f8a5373aa46c62634
williamabreu <williamsantos_00@hotmail.com>
2020-05-27 23:46:53 -0300

    Normalized cgnat file
    To get prepared to make a OO module.

--- cgnat.py
+++ cgnat.py
@@ -1,65 +1,61 @@
-# USO:
-#       python3 cgnat.py <CGNAT-SUBNET> <PUBLIC-SUBNET> 
-# EXEMPLO:
-#       python3 cgnat.py 100.64.40.0/22 179.106.165.192/27 > CGNAT.rsc
+from vlsm import split_subnet
+from ipaddress import IPv4Network
 
 
-import sys
-import ipaddress
+def main(private_net: IPv4Network, public_net: IPv4Network) -> str:
+    """Generate CGNAT rules for RouterOS.
+    
+    Args:
+        private_net: Private address pool from CGNAT shared space address.
+        public_net: Public adddress pool target from netmap.
+    
+    Returns:
+        RouterOS CLI commands to run via telnet through copy-paste.
 
+    Raises:
+        ValueError: When the networks given do not satisfy the 1:32 ratio.
+    """
 
-def split_subnet(subnet, netmask):
-    if subnet.netmask == netmask:
-        return [subnet.exploded]
-    else:
-        branches = list(subnet.subnets())
-        return split_subnet(branches[0], netmask) + split_subnet(branches[1], netmask)
+    if public_net.prefixlen - private_net.prefixlen != 5:
+        raise ValueError('Only works to netmaps for 1:32 CGNAT ratio')
 
+    private_subnet_list = split_subnet(private_net, public_net.netmask)
 
-def main(private_ips, public_ip):
     string = '/ip firewall nat\n'
 
     port_index = 1536
     comment = True
 
-    for private_ip in private_ips:
+    for private_subnet in private_subnet_list:
         first_port = port_index
         last_port = port_index + 1999
         if comment:
-            string += f'add action=netmap chain=srcnat protocol=tcp src-address={private_ip} to-addresses={public_ip} to-ports={first_port}-{last_port} disabled=yes comment="CGNAT TCP {public_ip}"\n'
+            string += f'add action=netmap chain=srcnat protocol=tcp src-address={private_subnet} to-addresses={public_net} to-ports={first_port}-{last_port} disabled=yes comment="CGNAT TCP {public_net}"\n'
             comment = False
         else:
-            string += f'add action=netmap chain=srcnat protocol=tcp src-address={private_ip} to-addresses={public_ip} to-ports={first_port}-{last_port} disabled=yes\n'
+            string += f'add action=netmap chain=srcnat protocol=tcp src-address={private_subnet} to-addresses={public_net} to-ports={first_port}-{last_port} disabled=yes\n'
         port_index += 2000
 
     port_index = 1536
     comment = True
 
-    for private_ip in private_ips:
+    for private_subnet in private_subnet_list:
         first_port = port_index
         last_port = port_index + 1999
         if comment:
-            string += f'add action=netmap chain=srcnat protocol=udp src-address={private_ip} to-addresses={public_ip} to-ports={first_port}-{last_port} disabled=yes comment="CGNAT UDP {public_ip}"\n'
+            string += f'add action=netmap chain=srcnat protocol=udp src-address={private_subnet} to-addresses={public_net} to-ports={first_port}-{last_port} disabled=yes comment="CGNAT UDP {public_net}"\n'
             comment = False
         else:
-            string += f'add action=netmap chain=srcnat protocol=udp src-address={private_ip} to-addresses={public_ip} to-ports={first_port}-{last_port} disabled=yes\n'
+            string += f'add action=netmap chain=srcnat protocol=udp src-address={private_subnet} to-addresses={public_net} to-ports={first_port}-{last_port} disabled=yes\n'
         port_index += 2000
 
     comment = True
 
-    for private_ip in private_ips:
+    for private_subnet in private_subnet_list:
         if comment:
-            string += f'add action=netmap chain=srcnat src-address={private_ip} to-addresses={public_ip} disabled=yes comment="CGNAT ICMP {public_ip}"\n'
+            string += f'add action=netmap chain=srcnat src-address={private_subnet} to-addresses={public_net} disabled=yes comment="CGNAT ICMP {public_net}"\n'
             comment = False
         else:    
-            string += f'add action=netmap chain=srcnat src-address={private_ip} to-addresses={public_ip} disabled=yes\n'
+            string += f'add action=netmap chain=srcnat src-address={private_subnet} to-addresses={public_net} disabled=yes\n'
     
     return string
-
-
-if __name__ == '__main__':
-    cgnat_net = ipaddress.IPv4Network(sys.argv[1])
-    public_ip = ipaddress.IPv4Network(sys.argv[2])
-    private_ips = split_subnet(cgnat_net, public_ip.netmask)
-    result = main(private_ips, public_ip)
-    print(result, end='')
