62b8c4f5f5b7b2e766eafda660179dc6797cc853
William Abreu <william@t10.digital>
2021-10-17 13:47:38 -0300

    Updated setuptools

--- pycgnat/__init__.py
+++ pycgnat/__init__.py
@@ -1,1 +1,4 @@
-from .settings import __version__
+# Define the package's version.
+# - Is used for indexing and distributing the package
+# - Follow the conventions defined by PEP440
+__version__ = "1.0.0"
--- setup.py
+++ setup.py
@@ -1,45 +1,42 @@
-# Usage: 
-#
-# -- Build the project for distribution:
-#   python3 setup.py sdist bdist_wheel
-# 
-# -- Run unit tests:
-#   python3 setup.py test
-
-
-from pycgnat import __version__
+import pycgnat as src
 import setuptools
+import toml
 
+config = toml.load("pyproject.toml")["tool"]["poetry"]
+config["author"] = ", ".join(
+    [" ".join(name_email.split()[:-1]) for name_email in config["authors"]]
+)
+config["author_email"] = ", ".join(
+    [name_email.split()[-1][1:-1] for name_email in config["authors"]]
+)
 
 setuptools.setup(
-    name='pycgnat',
-    version=__version__,
-    license='MIT License',
-    author='William Abreu',
-    author_email='contato@williamabreu.net',
-    description='Python module for generating CGNAT rules using netmap',
-    long_description=open('README.md').read(),
-    long_description_content_type='text/markdown',
-    url='https://github.com/williamabreu/py-cgnat',
-    install_requires=open('requirements.txt').read().splitlines(),
-    platforms='any',
-    packages=setuptools.find_packages('.', exclude=('pycgnat.tests',)),
-    python_requires='>=3.7',
-    test_suite='pycgnat.tests',
-    keywords='netmap cgnat rfc6598 routeros',
+    name=config["name"],
+    version=config["version"],
+    license=config["license"],
+    author=config["author"],
+    author_email=config["author_email"],
+    description=config["description"],
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/williamabreu/py-cgnat",
+    install_requires=open("requirements.txt").read().splitlines(),
+    platforms="any",
+    packages=setuptools.find_packages(".", exclude=(f"{src.__name__}.tests",)),
+    python_requires=">=3.7",
+    test_suite=f"{src.__name__}.tests",
+    keywords="netmap cgnat rfc6598 routeros",
     entry_points={
-        'console_scripts': [
-            'pycgnat=pycgnat.__main__:main'
-        ]
+        "console_scripts": [f"pycgnat={src.__name__}.__main__:main"]
     },
     classifiers=[
-        'Environment :: Console',
-        'Intended Audience :: System Administrators',
-        'Programming Language :: Python :: 3 :: Only',
-        'License :: OSI Approved :: MIT License',
-        'Operating System :: OS Independent',
-        'Topic :: Software Development :: Libraries :: Python Modules',
-        'Topic :: System :: Networking :: Firewalls',
-        'Topic :: Utilities',
+        "Environment :: Console",
+        "Intended Audience :: System Administrators",
+        "Programming Language :: Python :: 3 :: Only",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Topic :: System :: Networking :: Firewalls",
+        "Topic :: Utilities",
     ],
 )
